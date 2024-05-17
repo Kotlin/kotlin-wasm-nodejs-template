66136684454020fd418c7ae106c249c20e6a0c90
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-05-17 13:37:33 +0200

    Prepare for user projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
+import java.io.OutputStream
 
 plugins {
-    kotlin("multiplatform") version "1.9.22"
+    kotlin("multiplatform")
 }
+
+val kotlin_repo_url: String? = project.properties["kotlin_repo_url"] as String?
 
 repositories {
     mavenCentral()
+    kotlin_repo_url?.also { maven(it) }
 }
 
 kotlin {
@@ -17,11 +19,29 @@ kotlin {
     }
 }
 
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-}
+tasks.withType<NodeJsExec>().all {
 
-tasks.withType<org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask>().configureEach {
-    args.add("--ignore-engines")
+    val result = StringBuilder()
+
+    standardOutput = object : OutputStream() {
+        override fun write(b: Int) {
+            result.append(b.toChar())
+        }
+
+    }
+
+    doLast {
+        println(result.toString())
+
+        val expectedString1 = "Hello from Kotlin/Wasm"
+
+        val lines = result.lines().filter { it.isNotEmpty() }
+        check(lines.size == 1) {
+            "Expected 1 lines, actual: ${lines.size}"
+        }
+
+        check(lines[0] == expectedString1) {
+            "Expected '$expectedString1', actual: '${lines[0]}'"
+        }
+    }
 }
