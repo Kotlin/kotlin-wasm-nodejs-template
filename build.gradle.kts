a3e01129734b1cddfaa24852fcbc864ee755a9af
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-05-28 16:15:59 +0200

    Add language_version option

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,57 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.Companion.fromVersion
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
+import java.io.OutputStream
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
 }
 
+val kotlin_repo_url: String? = project.properties["kotlin_repo_url"] as String?
+val language_version: String? = project.properties["language_version"] as String?
+
 repositories {
     mavenCentral()
+    kotlin_repo_url?.also { maven(it) }
 }
 
 kotlin {
     wasmJs {
         binaries.executable()
         nodejs()
+
+        compilations.configureEach {
+            compileTaskProvider.configure {
+                language_version?.let {
+                    compilerOptions.languageVersion.set(fromVersion(it))
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<NodeJsExec>().all {
+
+    val result = StringBuilder()
+
+    standardOutput = object : OutputStream() {
+        override fun write(b: Int) {
+            result.append(b.toChar())
+        }
+
+    }
+
+    doLast {
+        println(result.toString())
+
+        val expectedString1 = "Hello from Kotlin/Wasm"
+
+        val lines = result.lines().filter { it.isNotEmpty() }
+        check(lines.size == 1) {
+            "Expected 1 lines, actual: ${lines.size}"
+        }
+
+        check(lines[0] == expectedString1) {
+            "Expected '$expectedString1', actual: '${lines[0]}'"
+        }
     }
 }
